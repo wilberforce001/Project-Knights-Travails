f79b298d99743f5e5285756017505fcfe79ede9d
wilberforce001 <kenmisik@gmail.com>
2024-03-13 19:38:49 +0300

    Called the KnightMoves function with different starting and ending points and logged the resultto the console

--- knights.js
+++ knights.js
@@ -35,3 +35,8 @@ function knightMoves(start, end) {
 
 
 }
+
+console.log(knightMoves([0,0], [1,2]));
+console.log(knightMoves([0,0],[3,3]));
+console.log(knightMoves([3,3],[0,0]));
+console.log(knightMoves([0,0],[7,7]));
